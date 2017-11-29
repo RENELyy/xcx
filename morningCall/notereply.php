ac60449be0348bd20cdd1923f04590055c43b7a2
RENELyy <411192513@qq.com>
2017-11-29 14:30:11 +0800

    gai reply id

--- morningCall/notereply.php
+++ morningCall/notereply.php
@@ -4,7 +4,7 @@ $sql = "select * from `reply`";
 $result = mysqli_query($link,$sql);
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
-        $output[] = array('nickName' => $row['nickName'],'name' => $row['name'],'content' => $row['content'],'todaytime' => $row['todaytime'],'view' => $row['view']);
+        $output[] = array('id' => 1,'nickName' => $row['nickName'],'name' => $row['name'],'content' => $row['content'],'todaytime' => $row['todaytime'],'view' => $row['view']);
     }
 }
 print_r(json_encode($output));
