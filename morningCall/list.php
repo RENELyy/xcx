b52e38dc44dce9b0d8639358a16a962c04ee8414
RENELyy <411192513@qq.com>
2017-11-30 17:49:10 +0800

    xiugai list Pid

--- morningCall/list.php
+++ morningCall/list.php
@@ -6,7 +6,7 @@ $sql = "select * from `content` order by `id` desc";
 $result = mysqli_query($link,$sql);
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
-        $output[] = array('nickName' => $row['nickName'],'content' => $row['content'],'imageUrl' => $row['imageUrl'],'todaydate' => $row['todaydate'],'todaytime' =>$row['todaytime'],'Pid' => $orw['Pid']);
+        $output[] = array('nickName' => $row['nickName'],'content' => $row['content'],'imageUrl' => $row['imageUrl'],'todaydate' => $row['todaydate'],'todaytime' =>$row['todaytime'],'Pid' => $row['Pid']);
     }
 }
 print_r(json_encode($output));
